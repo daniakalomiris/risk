dca3588b34152b4352c87f0e44a20441699da17d
Maelle <guoxi451@hotmail.com>
2019-11-01 00:04:18 -0400

    First iteration on fortification

--- Risk/Player.cpp
+++ Risk/Player.cpp
@@ -56,7 +56,13 @@ void Player::setName(string name) {
 	this->name = name;
 }
 
+vector<Country*> Player:: getThisPlayerCountries() {
+    return countries;
+}
 
+void Player:: setThisPlayerCountry(Country* country) {
+    countries.push_back(country);
+}
 
 //extra methods , reinforce, attack and fortify, will be implemented in next iteration
 void Player::reinforce() {
@@ -70,7 +76,59 @@ void Player::attack() {
 
 //fortify method
 void Player::fortify() {
-	cout << "The player fortify" << endl;
+    string nameSourceCountry;
+    string nameTargetCountry;
+    bool isSourceCountryValid = false;
+     bool isTargetCountryValid = false;
+    bool isNumOfArmiesValid = true;
+
+ 
+    int numOfArmies;
+    int indexOfSourceCountry;
+    
+    Country sourceCountry;
+    Country targetCountry;
+    
+    cout << "Please write the name of the chosen source country (capitalize the first letter)" << endl;
+    cin >> nameSourceCountry;
+    
+    
+    //check if the player owns the source country
+    for(int i = 0; i < this->getThisPlayerCountries().size(); i++) {
+        if( nameSourceCountry.compare(this->getThisPlayerCountries().at(i)->getCountryName()) == 0) {
+            isSourceCountryValid = true;
+            indexOfSourceCountry = i;
+        }
+    }
+    
+    cout << "Please enter the number of armies you would like to move" << endl;
+    cin >> numOfArmies;
+    
+    
+    if(numOfArmies < 1 || numOfArmies > this->getThisPlayerCountries().at(indexOfSourceCountry)->getNumberOfArmies() -1 ) {
+        isNumOfArmiesValid = false;
+    }
+    
+    
+    cout << "Please write the name of the chosen target country (capitalize the first letter)" << endl;
+       cin >> nameTargetCountry;
+    
+    //check if the player owns the target country
+       for(int i = 0; i < this->getThisPlayerCountries().size(); i++) {
+           if( nameTargetCountry.compare(this->getThisPlayerCountries().at(i)->getCountryName()) == 0) {
+               isSourceCountryValid = true;
+               indexOfSourceCountry = i;
+           }
+       }
+    
+    cout << "Is the source country valid? " <<  std::boolalpha << isSourceCountryValid << endl;
+	    
+    cout << "Is the source number of armies valid? " <<  std::boolalpha << isNumOfArmiesValid << endl;
+    cout << "The player fortify" << endl;
+    
+    //i still need to check if the source is adjacent to the target
+    //if the number of armies is valid, i need to decrement the number of armies in source and increment in target
+    //loop everything until all values are good
 }
 
 
