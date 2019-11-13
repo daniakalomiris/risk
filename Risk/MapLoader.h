e3a8f27b50a4f66ab5e51476258f8f5a048c4d11
Maelle <guoxi451@hotmail.com>
2019-11-12 21:37:49 -0500

    added maploaderconquest class and adapter class

--- Risk/MapLoader.h
+++ Risk/MapLoader.h
@@ -24,3 +24,53 @@ private:
     std::vector<std::string> borders_info;
     
 };
+
+
+class MapLoaderConquest {
+    
+public:
+    //constructor
+    MapLoaderConquest();
+    
+    //destructor
+    ~MapLoaderConquest();
+    
+    void readConquestMapFile();
+    void createConquestMap();
+    void displayConquestMap();
+    Map* getMap();
+
+    private:
+    Map* map;
+    
+    //vectors of information from conquest map file
+    std::vector<std::string*> continents_info;
+    std::vector<std::string*> territories_info;
+    
+    
+};
+
+
+class adapterMap: public MapLoader {
+    
+    public:
+    //constructor with parameter
+    adapterMap(MapLoaderConquest* conquestMap);
+    
+    //default constructor
+    adapterMap();
+    
+    //destructor
+    ~adapterMap();
+    
+    void readMapFile(std::string fileName);
+    void createMap();
+    void displayMap();
+    Map* getMap();
+    
+private:
+    Map* map;
+    MapLoaderConquest* conquestMaploader;
+
+
+};
