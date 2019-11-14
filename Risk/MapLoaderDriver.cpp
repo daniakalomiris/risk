777a61ba1e7724019c5b30aa7a97a9f2015ca76c
Maelle <guoxi451@hotmail.com>
2019-11-14 11:40:49 -0500

    reading conquest map

--- Risk/MapLoaderDriver.cpp
+++ Risk/MapLoaderDriver.cpp
@@ -4,18 +4,41 @@
 using namespace std;
 
 
-int main(){
-	string mapName;
-	MapLoader myMapLoader;
-	cout << "Please enter the name of the map you would like to load" << endl;
-	cin >> mapName;
+int main() {
+        string mapName;
+    MapLoaderConquest* myMapLoaderConquest = new MapLoaderConquest();;
+        cout << "Please enter the name of the map you would like to load" << endl;
+        
+        //use getlines so we can enter file names with spaces
+        getline(cin, mapName);
+   
+        myMapLoaderConquest->readConquestMapFile("maps/conquest/" + mapName + ".map");
+    myMapLoaderConquest->createConquestMap();
+    myMapLoaderConquest->displayConquestMap();
 
-	myMapLoader.readMapFile("maps/" + mapName + ".map");
-    myMapLoader.createMap();
-    myMapLoader.displayMap();
-	cout << "End file" << endl;
-
-
+        cout << "End file" << endl;
+    
+    delete myMapLoaderConquest;
+    myMapLoaderConquest = NULL;
+    
 }
+
+
+
+
+//Driver for A1
+//int main(){
+//	string mapName;
+//	MapLoader myMapLoader;
+//	cout << "Please enter the name of the map you would like to load" << endl;
+//	cin >> mapName;
+//
+//	myMapLoader.readMapFile("maps/" + mapName + ".map");
+//    myMapLoader.createMap();
+//    myMapLoader.displayMap();
+//	cout << "End file" << endl;
+//
+//
+//}
 
 
