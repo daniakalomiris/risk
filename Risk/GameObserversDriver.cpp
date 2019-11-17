3bab047a9d528c7b4ae8779bf809fc89acf1add3
Jen Younanian <jyounanian@hotmail.com>
2019-11-16 19:09:59 -0500

    modified table

--- Risk/GameObserversDriver.cpp
+++ Risk/GameObserversDriver.cpp
@@ -13,6 +13,7 @@ int main() {
     //create own fictive for testing the driver
     Map* map = new Map();
     
+	//creates new continent objects
     Continent* continent1 = new Continent();
     Continent* continent2 = new Continent();
     Continent* continent3 = new Continent();
@@ -21,15 +22,17 @@ int main() {
     continent2->setContinentName("Central America");
     continent3->setContinentName("South America");
     
+	//set number of armies per continent
     continent1->setNumberOfArmies(10);
     continent2->setNumberOfArmies(7);
     continent3->setNumberOfArmies(9);
     
+
     continent1->setAdjacentContinents(continent2);
     continent2->setAdjacentContinents(continent1);
     continent2->setAdjacentContinents(continent3);
         
-    
+    //creating countries
     Country* country1 = new Country();
     Country* country2 = new Country();
     Country* country3 = new Country();
@@ -44,6 +47,7 @@ int main() {
     country5->setCountryName("Peru");
     country6->setCountryName("Brazil");
     
+	//setting adjacent countries
     country1->setAdjacentCountries(country2);
     country1->setAdjacentCountries(country6);
     country2->setAdjacentCountries(country1);
@@ -58,6 +62,7 @@ int main() {
       country6->setAdjacentCountries(country1);
     
     
+	//setting countries to continents
     continent1->setCountriesOfContinent(country1);
     continent1->setCountriesOfContinent(country2);
     continent2->setCountriesOfContinent(country3);
@@ -65,6 +70,7 @@ int main() {
     continent3->setCountriesOfContinent(country5);
     continent3->setCountriesOfContinent(country6);
     
+	//setting continents to countries
     country1->setContinentOfCountry(continent1);
     country2->setContinentOfCountry(continent1);
     country3->setContinentOfCountry(continent2);
@@ -72,6 +78,7 @@ int main() {
     country5->setContinentOfCountry(continent3);
     country6->setContinentOfCountry(continent3);
     
+	//creating continents and maps 
     map->setContinent(continent1);
     map->setContinent(continent2);
     map->setContinent(continent3);
@@ -82,6 +89,7 @@ int main() {
     map->setCountry(country5);
     map->setCountry(country6);
     
+	//passing the created map, countries, players to the concrete subject (game engine)
     theGame->setMap(map);
 	theGame->askNumberOfPlayers();
 	theGame->createPlayers();
@@ -98,10 +106,35 @@ int main() {
 		}
 	}
 
+	//passing concrete subject to observer
     PlayerDominationView* pdv = new PlayerDominationView(theGame);
     
     theGame->mainGameLoop();
      
+//deallocating pointers 
 delete theGame;
+delete map;
+delete continent1;
+delete continent2;
+delete continent3;
+delete country1;
+delete country2;
+delete country3; 
+delete country4;
+delete country5;
+delete country6; 
+
+
 theGame = NULL;
+map = NULL;
+continent1 = NULL;
+continent2 = NULL;
+continent3 = NULL;
+country1 = NULL;
+country2 = NULL;
+country3 = NULL;
+countr4 = NULL;
+country5 = NULL;
+country6 = NULL;
+
 }
